b5800f82f69a029ecf743bbca70bae3be2b187b8
Jose Manuel Cruz <jose.cruz.martinez.al@iespoligonosur.org>
2026-09-16 16:45:30 +0200

    Subido la carpeta

---
+++ unidad 1/ud1/src/main/java/ud1/Main.java
@@ -0,0 +1,28 @@
+package ud1;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File f1 = new File("C:\\Users\\User\\Desktop\\text.txt");
+        File f2 = new File("f.txt");
+
+        File f3 = new File("/home/alumnadotarde/Escritorio/ACCESO A DATOS/unidad 1", "fichero1.txt");
+        File f4 = new File("/home/alumnadotarde/Escritorio/ACCESO A DATOS/unidad 1/fichero1.txt");
+        File f5 = new File("./ACCESO A DATOS/unidad 1/fichero1.txt"); //*****/
+
+        File d = new File("./datos");
+
+
+        System.out.println(f3.exists());
+        System.out.println(f4.exists());
+
+        System.out.println(File.separator);
+
+        File[] files = d.listFiles();
+
+        for (File file : files){
+            System.out.println(file.getName());
+        }
+    }
+}
